e1b579894f5c3f1f049580ab25e3b36138ef3e3d
Hasan Ali <hasancseiu16@gmail.com>
2023-06-02 12:12:22 +0600

    added server in index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log('hasan');
-const hasan = 29;
-console.log(hasan);
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.send("Hasan");
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Your app is running on port 5000');
+});
