15c1e242662ac3287a41d42b7d5fb023b150eecb
Hasan Ali <hasancseiu16@gmail.com>
2023-06-03 21:24:41 +0600

    added Header.js file

--- front_app/.next/static/chunks/webpack.js
+++ front_app/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "042accee75f83a63"; }
+/******/ 		__webpack_require__.h = function() { return "20e92f9049af37c6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
