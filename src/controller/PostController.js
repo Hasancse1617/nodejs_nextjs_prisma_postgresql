45fde590d20bd25a910fa12edd0d629d64c07c56
Hasan Ali <hasancseiu16@gmail.com>
2023-06-09 13:39:09 +0600

    updated allPost api in postController.js

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -8,8 +8,8 @@ module.exports.allPost = async(req, res)=>{
     const skip = (page - 1) * perPage;
     
     try {
-        const count = await Post.count();
-        const response = await Post.findAll({offset: skip, limit: perPage, order:[['updatedAt','DESC']]});
+        const count = await prisma.post.count();
+        const response = await prisma.post.findMany({skip: skip, take: perPage, orderBy:[{updatedAt:'desc'}]});
         return res.status(200).json({response, count, perPage});
     } catch (error) {
         return res.status(500).json({errors: [{msg: error.message}]});
