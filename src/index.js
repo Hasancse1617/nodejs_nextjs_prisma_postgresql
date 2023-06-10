97bfb74d4c0389e6ccac2b2003367a4fc2205d1a
Hasan Ali <hasancseiu16@gmail.com>
2023-06-10 14:35:51 +0600

    added comment router & controller

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const router = require("./routes/Router");
+const postrouter = require("./routes/postRouter");
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
-app.use("/api", router);
+app.use("/api", postrouter);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Your app is running on port 5000');
